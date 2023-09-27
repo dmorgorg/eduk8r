83dc5434d3bcfb8e7747c4f00e62dd50ed48bb0c
dave morgan <dave@dmorg.org>
2023-09-27 16:27:58 -0600

    initial file structure

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,19 @@
+<nav>
+  <ul>
+    <li>
+      <a href="/about">About</a>
+    </li>
+    <li>
+      <a href="/water">water</a>
+    </li>
+    <li>
+      <a href="/statics">statics</a>
+    </li>
+    <li>
+      <a href="/strength">strength</a>
+    </li>
+    <li>
+      <a href="/misc">misc</a>
+    </li>
+  </ul>
+</nav>
